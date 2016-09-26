861248abb6f0beef4463814fe6e02a658ea30b2d
sarahmorley <s.morley2@hotmail.com>
2016-09-26 18:05:48 +0100

    Add files via upload

---
+++ Leap Year.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Design_and_Programming_Assignment
+{
+    class Leap_Year
+    {
+        static void Main(string[] args)
+        {
+            // declaration of variables
+            int year = 0;
+            bool valid = false;
+
+            // while loop statement until valid year format 
+            while (!valid)
+            {
+                // prompt user to enter a year
+                Console.WriteLine("Enter a Year (YYYY):");
+
+                // try parse to int, catch if cannot parse to int (eg. user entered letter) and assign 0, 
+                // this way program won't crash
+                try
+                {
+                    year = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    year = 0;
+                }
+                // if valid year entered move on, else prompt user that year entered is invalid
+                if ((year >= 1000) && (year <= 9999))
+                {
+                    valid = true;
+                }
+                else
+                {
+                    Console.WriteLine("You have enter an invalid year format");
+                }
+            }
+            // calculation = if year is evenly divisable by 4, expect if evenly divisable by 100,
+            // expect if evenly divisable by 400
+            if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+            {             
+                Console.WriteLine("The year {0} is a leap year", year);
+            }
+            else
+            {              
+                Console.WriteLine("The year {0} is not a leap year", year);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Secret Number.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Design_and_Programming_Assignment
+{
+    class SecretNumber
+    {
+        static void Main(string[] args)
+        {
+            // declartion of variables
+            Random random = new Random();
+            int secretNum = random.Next(0, 20); 
+            int guess = 0;
+
+            // start of for loop
+            // decalre int attempt and set as 0 (initial condition), 
+            // while attempt is less that 3 (test condition),
+            // increment attempt by 1 (update condition).
+            for (int attempt = 0; attempt < 3; attempt++)
+            {
+                // Prompt user to guess a number and try parse to int guess, catch if letter entered and display error. 
+                Console.WriteLine("Try to guess the secret number between 0 and 20. You have {0} attempt(s) left.", 3 - attempt);
+                try
+                {
+                    guess = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Error: You must enter a number.\n");
+                    continue;  // count as an attempt and skip to the next iteration in the loop
+                }
+                // if guess is correct display and break out of loop
+                if (guess == secretNum)
+                {
+                    Console.WriteLine("Well done, you guessed correctly.");
+                    break;  // break out of loop now
+                }
+                // else if guess is less than 5 display
+                else if (guess < secretNum)
+                {
+                    Console.WriteLine("Your guess is too low.\n");
+                }
+
+                // else if guess is greater than 5 display
+                else if (guess > secretNum)
+                {
+                    Console.WriteLine("Your guess is too high.\n");
+                }
+            }
+            // after 3 attempts if guess is not correct display
+            if (guess != secretNum)
+            {
+                Console.WriteLine("Hard luck, the number was {0}", secretNum);
+            }
+            Console.ReadLine();
+        }
+    }
+}
