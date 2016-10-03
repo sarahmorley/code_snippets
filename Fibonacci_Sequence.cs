a4d29d5d2d7ad53c7c92605f78752a70e7a3641c
Sarah Morley <s.morley2@hotmail.com>
2016-10-03 15:18:14 +0100

    Add files via upload

---
+++ Fibonacci_Sequence.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // print out the Fibonacci Sequence - length determined by user input
+
+            Console.WriteLine($"Enter the length of the Fibonacci sequence you want:");
+            int length = int.Parse(Console.ReadLine());
+
+            int a = 0, b = 1, c = 0;
+            Console.WriteLine($"{a}, {b}");
+
+            for (int i = 0; i < length; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                Console.WriteLine($"{c}");
+            }
+
+            /* print out nth number in the Fibonacci sequence by calling the method below 
+            nth number determined by user input*/
+
+            Console.WriteLine($"Enter the number in the Fibonacci sequence you wish to access");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetNthFibonacciNo(n));
+        }
+
+
+            // method to print out the nth number in the Fibonacci sequence 
+
+            public static int GetNthFibonacciNo (int n)
+        {
+            int number = n - 1;
+            int[] fibonacci = new int[number + 1];
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+            
+            for(int i = 2; i <= number; i++)
+            {
+                fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+            }
+
+            return fibonacci[number];
+        }
+ 
+        
+    }
+}
