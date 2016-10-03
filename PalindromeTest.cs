590bfafb63678f67a1aede0691cb19b4feac5323
Sarah Morley <s.morley2@hotmail.com>
2016-10-03 20:48:44 +0100

    Add files via upload
    This is a program to test if a word is a Palindrome or not.

---
+++ PalindromeTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            Console.WriteLine($"Enter the word you wish to test: ");
+            string str = Console.ReadLine();
+ 
+            Console.WriteLine(IsPalindrome(str));             
+                       
+       }
+
+
+        public static bool IsPalindrome(string str)
+        {
+            for(int x = str.Length -1, y = 0; x > y; x--, y++)
+            {
+                if (str[x] != str[y])
+                    return false;
+            }
+
+            return true; 
+        }   
+
+       
+    }
+}
