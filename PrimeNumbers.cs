d94d7263bf7ac2e472eb61c1767fea9429a421f9
Sarah Morley <s.morley2@hotmail.com>
2016-10-11 15:32:33 +0100

    Add files via upload

---
+++ PrimeNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int x = 2; x < 100; x++)
+            {
+                int isPrime = 0;
+                for (int y = 1; y < x; y++)
+                {
+                    if (x % y == 0)
+                        isPrime++;
+
+                    if (isPrime == 2) break;
+                }
+                if (isPrime != 2)
+                    Console.WriteLine(x);
+
+                isPrime = 0;
+            }
+            
+        }
+    }
+}
