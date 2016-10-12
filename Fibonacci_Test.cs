62836a89e5e7984de579b03dc30e856c3d3cc494
Sarah Morley <s.morley2@hotmail.com>
2016-10-12 18:17:59 +0100

    Add files via upload

---
+++ Fibonacci_Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using fib2;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestNumberInFibSequence()
+        {
+            int number = 8;
+            int seventhInSequence = Fib.GetNthFibonacciNo(7);
+            Assert.AreEqual(seventhInSequence, number);
+        }
+    }
+}
