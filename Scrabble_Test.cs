6a0701485dd9e821819b4b91ac0d1d4ab9797bae
Sarah Morley <s.morley2@hotmail.com>
2016-10-13 11:46:10 +0100

    Add files via upload

---
+++ Scrabble_Test.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScrabbleScore; 
+
+namespace ScrabbleTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCorrectScoreforWord()
+        {
+            int score = 8;
+            int wordScore = Scrabble.GetLetterPoints("hello");
+            Assert.AreEqual(score, wordScore);
+        }
+
+        [TestMethod]
+        public void TestCorrectScoreforLetter()
+        {       
+            int score = 10;
+            int wordScore = Scrabble.GetLetterPoints("z");
+            Assert.AreEqual(score, wordScore);
+        }
+
+        [TestMethod]
+        public void TestEmptyWordScoresZero()
+        {
+            string empty = "";
+            int score = 0;
+            int wordScore = Scrabble.GetLetterPoints(empty);
+            Assert.AreEqual(score, wordScore);
+        }
+
+
+
+    }
+}
