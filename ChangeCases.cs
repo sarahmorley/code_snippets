3e9f6ac2453391816635905898246aac60f69590
sarah.morley <sarah.morley@enernoc.com>
2018-01-05 16:41:25 +0000

    add basic code snippet

---
+++ ChangeCases.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchLowerAndUpperCases
+{
+    class ChangeCases
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert and word or sentence with both upper and lower case letters.");
+            string word = Console.ReadLine();
+            int length = word.Length;
+            char[] letterArray = word.ToCharArray(0, length);
+            char letter;
+
+            for(int i = 0; i < length; i++)
+            {
+                letter = letterArray[i];
+                if(Char.IsLower(letter))
+                {
+                    Console.Write(Char.ToUpper(letter));
+                }
+                else
+                {
+                    Console.Write(Char.ToLower(letter));
+                }
+            }
+        }
+    }
+}
