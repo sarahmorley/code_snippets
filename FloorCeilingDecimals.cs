d7c004e1930dbe3ec68973d277a250d35258c9ab
sarah.morley <sarah.morley@enernoc.com>
2018-01-05 15:29:53 +0000

    add new basic snippets

---
+++ FloorCeilingDecimals.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloorAndCeilingDecimals
+{
+    class RoundDecimals
+    {
+        static void Main(string[] args)
+        {
+            decimal decimalNumber;
+
+            Console.WriteLine("Insert a decimal");
+            decimalNumber = Decimal.Parse(Console.ReadLine());
+
+            var flooredDecimal = Decimal.Floor(decimalNumber);
+            var ceilingedDecimal = Decimal.Ceiling(decimalNumber);
+
+            Console.WriteLine("Decimal: {0}, Floor: {1}, Ceiling {2}", decimalNumber, flooredDecimal, ceilingedDecimal);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ReverseAWordManually.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseAWord
+{
+    class ReverseWord
+    {
+        static void Main(string[] args)
+        {
+            
+            //int length;
+            string answer = "";
+
+            do
+            {
+                string word;
+                string reverseWord = "";
+
+                Console.WriteLine("Enter the word you wish to reverse.");
+                word = Console.ReadLine();
+
+                //length = word.Length - 1;
+
+                //while (length >= 0)
+                //{
+                //    reverseWord = reverseWord + word[length];
+                //    length--;
+                //}
+
+                foreach(char letter in word)
+                {
+                    reverseWord = letter + reverseWord;
+                }
+
+                Console.WriteLine("{0} reversed is {1}", word, reverseWord);
+
+                Console.WriteLine("Would you like to reverse another word? Enter 'Y' for yes of any other key for No.");
+                answer = Console.ReadLine();
+
+            } while (answer == "Y");
+       
+        }
+    }
+}
